a4ba8583b0f97b6f78e24e1065510ac1e7a62c96
6182001018 - Benedictus Melvern <118327384+melvern23@users.noreply.github.com>
2023-12-14 22:01:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -58,6 +58,58 @@ app.get('/adddata', (req, res) => {
     res.render('AddData');
 });
 
+app.post ('/upload', uploads.single('csvFile'), (req, res) => {
+    console.log(req.file.path)
+    uploadCSV(__dirname + "/data/" + req.file.filename)
+    res.send("awikwok")
+});
+
+function uploadCSV (path) {
+    let stream = fs.createReadStream(path)
+    let CSVDataColl = []
+    let fileStream = csv
+    .parse({ delimiter: ';' })
+    .on('data', function(data){
+        if (data[4] === null || data[4] === '') {
+            data[4] = 0;
+        } else {
+            data[4] = parseFloat(data[4]);
+        }
+
+        const parsedDate = moment(data[7], 'DD/MM/YYYY', true);
+
+        if (parsedDate.isValid()) {
+            data[7] = parsedDate.format('YYYY-MM-DD');
+            CSVDataColl.push(data);
+        } else {
+            console.error(`Invalid date: ${data[7]}`);
+        }
+    })
+    .on('end', function(){
+        console.log("CSV Parsing completed");
+        CSVDataColl.shift()
+
+        pool.getConnection((error, connection) => {
+            if (error) {
+                console.log(error)
+            } else {
+                let query = "INSERT INTO Customer (ID, Year_Birth, Education,Marital_Status, Income, Kidhome, Teenhome, Dt_Customer, Recency,  MntWines, MntFruits, MntMeatProducts, MntFishProducts, MntSweetProducts, MntGoldProds, NumDealsPurchases, NumWebPurchases, NumCatalogPurchases, NumStorePurchases,NumWebVisitsMonth, AcceptedCmp3, AcceptedCmp4, AcceptedCmp5,AcceptedCmp1, AcceptedCmp2, Complain, Z_CostContact, Z_Revenue, Response) VALUES ?"
+                console.log(query);
+                connection.query(query, [CSVDataColl], (error, res) => {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log("Inserted rows: " + res.affectedRows);
+                    }
+                    connection.release();
+                })
+            }
+        })
+        fs.unlinkSync(path)
+    })
+    stream.pipe(fileStream)
+}
+
 app.get('/filterdata', (req, res) => {
     res.render('FilterData');
 });
